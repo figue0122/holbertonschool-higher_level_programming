f9c27e3501170e3fc31fdf087b2ca4309883a604
figue0122 <miguel.figueroa0122@gmail.com>
2023-09-29 13:13:19 -0700

    upgrade

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
+#!/user/bin/python3
+print("programming is like building a multilingual puzzle ")
 
-print("Programming is like building a multilingual puzzle ")
-=======
-#!/usr/bin/python3
-print("Programming is like building a multilingual puzzle ")
->>>>>>> bf2dd09503717ee0ae3863945958f0b1dc56e3b8
