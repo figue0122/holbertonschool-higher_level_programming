73f57e17ff60f1201710c238937cb8d9b7a45147
figue0122 <you@example.com>
2023-09-29 18:31:47 -0400

    fix f-strings  nad comppleted the funct

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,4 +7,8 @@ if number < 0:
 else:
     last_digit = number % 10
 if last_digit > 5:
-    
+    print(f"{beg_string} is {last_digit} and is greater than 5")
+elif last_digit == 0:
+    print(f"{beg_string} is {last_digit} and is 0")
+else:
+    print(f"{beg_string} is {last_digit} and is less than 6 and not 0")
