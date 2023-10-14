f74fa1f8b90f4b6b133fd413dfc24728368287f3
figue0122 <you@example.com>
2023-10-13 21:30:18 -0400

    2 square

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module has a square class"""
+
+
+class Square:
+    """A class to represent a square."""
+    def __init__(self, size=0):
+        """Initialize the square with a given size.
+
+        Args:
+        - size: The size of the square's sides.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
