0715b7a87424fee204da5495126fb93a5b397627
figue0122 <you@example.com>
2023-10-13 21:10:59 -0400

    square 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """This class defines a square"""
+
+    def __init__(self, size):
+        """Initializes data"""
+        self.__size = size
