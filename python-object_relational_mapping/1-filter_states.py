deba9c9acc7dffa76554755099dc83118c57ec2e
figue0122 <you@example.com>
2023-11-24 14:56:33 -0400

    ls all state with name from db hbt

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
+# Import necessary modules
 import MySQLdb
 import sys
 
+# Check if the script is being run as the main program
 if __name__ == "__main__":
+    # Establish a connection to the MySQL database
     cnx = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -12,17 +15,23 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         database=sys.argv[3])
 
+    # Create a cursor object for executing SQL queries
     cur = cnx.cursor()
 
+    # Execute a SELECT query to retrieve states with names starting with 'N'
     cur.execute(
         "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
 
+    # Fetch all rows from the executed query
     query_rows = cur.fetchall()
 
+    # Iterate through the query results and print states starting with 'N'
     for row in query_rows:
         if row[1][0] == 'N':
             print(row)
 
+    # Close the cursor to free up resources
     cur.close()
 
+    # Close the connection to the database
     cnx.close()
