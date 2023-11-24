cd6c89f26cccd60fc54fcdafec5910e891004772
figue0122 <you@example.com>
2023-11-24 16:15:49 -0400

    filter states 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+lists all states with a name starting with N from the database
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    u_name = argv[1]
+    p = argv[2]
+    db_name = argv[3]
+    search = argv[4]
+    """lists all states from the database hbtn_0e_0_usa"""
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=u_name, passwd=p,
+                         db=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}'\
+                 ORDER BY id".format(search))
+
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
+    cur.close()
+    db.close()
