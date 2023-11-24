2b67e4e05f1ab737f02e8392dd830d4986c14f68
figue0122 <you@example.com>
2023-11-24 12:27:38 -0400

    updae 4 first table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Makes a table with a id and name description the name will be
+-- received and it will not fail if the table already exists.
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
