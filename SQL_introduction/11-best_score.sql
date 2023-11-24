e97ce6717e7e5de1a27bb10e17df3c46f793ddd2
figue0122 <you@example.com>
2023-11-24 12:43:20 -0400

    selected the best

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Script that list records from a specific table in database
+-- based on a condition, displaying specific columns
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
