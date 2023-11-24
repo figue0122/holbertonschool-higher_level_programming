d9b68cb1f8063ae381942ce83d7f8e30f2d20d81
figue0122 <you@example.com>
2023-11-24 12:52:57 -0400

    Name

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- Script to list records from a specific table in database
+-- with specific conditions and sorting
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
