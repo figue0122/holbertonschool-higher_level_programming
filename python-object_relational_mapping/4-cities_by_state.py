6f8bce6a62967992f67eaf963fd395578eb7bcdc
figue0122 <you@example.com>
2023-11-25 15:14:12 -0400

    ls all cities on db

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module to list all cities from database hbtn_0e_4_usa
+"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """Function that lists all cities database hbtn_0e_4_usa"""
+
+    cnx = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        charset="utf8",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3])
+
+    cur = cnx.cursor()
+
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id ASC")
+
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+
+    cnx.close()
