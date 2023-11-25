bc86e9b52b24789124e2647fcd85d491db872e51
figue0122 <you@example.com>
2023-11-25 15:52:56 -0400

    model that ls all cities on db

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module to list all City objects from database
+"""
+
+
+from model_city import Base, City
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    """Function that lists all City objects from database"""
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.
+        format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    my_session = Session()
+
+    for city, state in my_session.query(City, State).\
+            filter(City.state_id == State.id).order_by(City.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    my_session.close()
