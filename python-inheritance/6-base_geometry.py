00c5271d183e78a152690e8cfa515a25a69a5537
figue0122 <you@example.com>
2023-11-29 15:43:12 -0400

    updated class geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create a class base geometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """unimplemented area"""
+        raise Exception("area() is not implemented")
