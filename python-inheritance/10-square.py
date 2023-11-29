d6d6505192848d3603ef04f223c8f6158ae64a6c
figue0122 <you@example.com>
2023-11-29 16:35:17 -0400

    square class module

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square class module"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inheriting rectangle"""
+
+    def __init__(self, size):
+        """initialize values"""
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """Area function"""
+        return self.__size * self.__size
