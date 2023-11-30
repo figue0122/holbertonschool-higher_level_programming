eda6a8243f1a927802999c82fbc98bc47d43d885
figue0122 <you@example.com>
2023-11-30 00:02:05 -0400

    module to add an item

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for add_item method."""
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    my_list = load_from_json_file("add_item.json")
+except:
+    my_list = []
+for i in sys.argv[1:]:
+    my_list.append(i)
+    save_to_json_file(my_list, "add_item.json")
