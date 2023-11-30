8386eb15d54a4eb628f9ad7f2c9f4ca01113b96c
figue0122 <you@example.com>
2023-11-29 23:59:34 -0400

    chmod  + added all main files

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for save_to_json_file method."""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """"writes an object to text file, using JSON representation."""
+    with open(filename, "w", encoding='utf-8') as f:
+        json.dump(my_obj, f)
